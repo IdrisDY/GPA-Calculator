50b8d73477a6e3187fc9586b3a21b7a2f08c17d5
Idris adekunle <idrisadekunle726@gmail.com>
2022-06-27 18:00:46 +0100

    added reset and calc function weback

--- calc.js
+++ calc.js
@@ -1,11 +1,12 @@
+let newmonth
 
 var x = document.getElementById("p")
 let container= Short("container")
 let newP =  document.createElement("div")
 container.appendChild(newP)
+let resetBtn = document.getElementsByClassName('btn-reset')[0]
 
-
-
+let g = 10
 
 function Reset(){
    container.reset()
@@ -13,6 +14,21 @@ function Reset(){
 function Short (tag){
    return document.getElementById(tag)
 }
+/*
+let P1=0
+let P2=0
+let P4=0
+let P3=0
+let P5=0
+*/
+var P1=0
+var P2=0
+var P3=0
+var P4=0
+var P5=0
+let TP = P1 +P2+P3+P4+P5
+console.log(TP)
+
 
 
  let inp2 = Short("Finp2")
@@ -37,53 +53,26 @@ let tinp3 = Short ("Tinp3")
 let Qinp1 = Short("Qinp1")
 let Qinp2 = Short ("Qinp2")
 let Qinp3 = Short ("Qinp3")
+  
+
+let TotalPoints=0
 
 
-
-Cinp3.onchange=function(event){
-   Cinp3.value= event.target.value
-   console.log(Cinp3.value)
-   let TotalUnits =Number(inp3.value) + Number(sinp3.value) + Number (tinp3.value) +Number(Qinp3.value) + Number(Cinp3.value)
-   Calc()}
-
-   Qinp3.onchange=function(event){
-      Qinp3.value= event.target.value
-      console.log(Qinp3.value)
-      let TotalUnits =Number(inp3.value) + Number(sinp3.value) + Number (tinp3.value) +Number(Qinp3.value) + Number(Cinp3.value)
-      Calc()}
-
-      tinp3.onchange=function(event){
-         tinp3.value= event.target.value
-         console.log(tinp3.value)
-         let TotalUnits =Number(inp3.value) + Number(sinp3.value) + Number (tinp3.value) +Number(Qinp3.value) + Number(Cinp3.value)
-         Calc()}
           
-inp3.onchange=function(event){
-inp3.value= event.target.value
-console.log(inp3.value)
-let TotalUnits =Number(inp3.value) + Number(sinp3.value) + Number (tinp3.value) +Number(Qinp3.value) + Number(Cinp3.value)
-Calc()
-}
 
    
-sinp3.onchange=function(event){
-sinp3.value= event.target.value
-console.log(sinp3.value)
-
-let TotalUnits =Number(inp3.value) + Number(sinp3.value) + Number (tinp3.value) +Number(Qinp3.value) + Number(Cinp3.value)
-Calc()
-}
 
 
 
 
 
    console.log("working")
-inp2.onchange=function(event){
-   inp2.value= event.target.value
+   function Calc(){
+console.log(allFilled)
+
    switch(inp2.value){
       case("A"):
-      console.log(   P1 = 5 *inp3.value); 
+      console.log( P1 = 5 *inp3.value); 
       break;
       case ("B"):
       console.log(P1 = 4*inp3.value);
@@ -101,47 +90,49 @@ inp2.onchange=function(event){
       console.log(P1=0*inp3.value);
       break;
             }
-    
-}
+            //TotalPoints=   P1+P2+P3+P4+P5
+
+
+
 
  
       // 2ND ROW
 
 
-sinp2.onchange=function(event){
-   sinp2.value= event.target.value
 
-   switch(Sinp2.value){
+
+  
+   switch(sinp2.value){
       case("A"):
-      console.log(   P2 = 5 *Sinp3.value); 
+      console.log(   P2 = 5 *sinp3.value); 
       break;
       case ("B"):
-      console.log(P2 = 4*Sinp3.value);
+      console.log(P2 = 4*sinp3.value);
       break;
       case("C"):
-      console.log(P2 =3*Sinp3.value);
+      console.log(P2 =3*sinp3.value);
       break;
       case ("D"):
-      console.log(P2=2*Sinp3.value);
+      console.log(P2=2*sinp3.value);
       break;
       case ("E"):
-      console.log(P2 =1*Sinp3.value);
+      console.log(P2 =1*sinp3.value);
       break;
       case ("F"):
-      console.log(P2=0*Sinp3.value);
+      console.log(P2=0*sinp3.value);
       break;
             }
-
+           // TotalPoints= TotalPoints + P2
+          // TotalPoints=   P1+P2+P3+P4+P5
             console.log (P2)
-}
-
-
+         
          // 3RD ROW 
 
 
 
-tinp2.onchange= function(event){
-   tinp2.value=event.target.value
+
+
+   
    switch(Tinp2.value){
       case("A"):
       console.log(  P3 = 5 *Tinp3.value); 
@@ -162,16 +153,16 @@ tinp2.onchange= function(event){
       console.log(P3=0*Tinp3.value);
       break;
             }
-            console.log (TotalPoints)
+            //TotalPoints= TotalPoints +P3
+          //  TotalPoints=   P1+P2+P3+P4+P5
    
-}
+
 
 //4TH ROW
+   
 
 
-
-Qinp2.onchange=function(event){
-   Qinp2.value=event.target.value
+  
 switch(Qinp2.value){
    case("A"):
    console.log(  P4 = 5 *Qinp3.value); 
@@ -192,18 +183,15 @@ switch(Qinp2.value){
    console.log(P4 =0*Qinp3.value);
    break;
          }
-   
+        // TotalPoints= TotalPoints + P4
+       // TotalPoints=   P1+P2+P3+P4+P5
          console.log (P4)
    
-
-}
+      
 //5TH ROW 
 
 
-Cinp2.onchange=function(event){
-   Cinp2.value=event.target.value
-
-
+   
    switch(Cinp2.value){
       case("A"):
       console.log(  P5 = 5 *Cinp3.value); 
@@ -224,18 +212,20 @@ Cinp2.onchange=function(event){
       console.log(P5 =0*Cinp3.value);
       break;
             }
-   
-}
+           // TotalPoints= TotalPoints+ P1 + P2+P3+P4+ P5
+          // TotalPoints= P1+P2+P3+P4+P5
 
 
-        
-
-   function Calc(){
+ 
       let TotalUnits =Number(inp3.value) + Number(sinp3.value) + Number (tinp3.value) +Number(Qinp3.value) + Number(Cinp3.value)
 
+
      
+    let TotalPoints= P1+P2+P3+P4+P5
+
       console.log(TotalUnits)
       console.log(TotalPoints)
+      console.log(P1)
       let FinalResult= TotalPoints/TotalUnits
       console.log (FinalResult.toFixed(2))
         
@@ -243,18 +233,31 @@ Cinp2.onchange=function(event){
           x.innerHTML=`your GP is ${FinalResult.toFixed(2)} out of an obtainable 5.00}`
     }
 
-/*function show(){
-   const node = document.createElement("p")
-   node.value="inp3.value"
-      const war = document.createTextNode('Water ') 
-      node.appendChild(war)
-     let die = document.getElementById("ninja")
-     die.appendChild( node)
-      const div2= document.getElementById("nin2")
-      div2.appendChild(die)
-   
+function resBtn(){
+   inp2.value =''
+   inp1.value= ''
+   inp3.value= ''
+  
+  
+  Cinp1.value ='' 
+  Cinp2.value =''
+  Cinp3.value ='' 
+  
+  
+  sinp1.value ='' 
+  sinp2.value ='' 
+  sinp3.value ='' 
+  
+  tinp1.value =''
+  tinp2.value =''
+  tinp3.value ='' 
+                 
+  
+  Qinp1.value ='' 
+  Qinp2.value =''
+  Qinp3.value ='' 
+    
 
-   ;
 }
- */
-      
+let allFilled =  Boolean(Qinp2.value != '')
+console.log(allFilled)
